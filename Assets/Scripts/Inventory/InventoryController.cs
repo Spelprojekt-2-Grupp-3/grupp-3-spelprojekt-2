bb249c330e66e00cc1153a30e16c49214e2e3376
LindGabriel <114293540+LindGabriel@users.noreply.github.com>
2025-02-04 13:20:16 +0100

    (semi)Working inventory
    Inventory is now capable of moving/storing 1x1 items.

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,20 +1,42 @@
 
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-
+//[RequireComponent(typeof(PlayerInput))]
 public class InventoryController : MonoBehaviour
 { 
    [HideInInspector]public ItemGrid selectedItemGrid;
 
+   private Shipment selectedPackage;
+   private RectTransform packageRectTransform;
+   
    private void Update()
    {
-      if(selectedItemGrid==null){return;}
+       //item drag
+       if (selectedPackage != null)
+           packageRectTransform.position = Input.mousePosition;
+       
+       if(selectedItemGrid==null){return;}
+      
+       //TODO:Implement input manager here
+       if (Mouse.current.leftButton.wasPressedThisFrame)
+       {
+           Vector2Int posOnGrid = selectedItemGrid.GetTileGridPosition(Input.mousePosition);
 
-
-      if (Input.GetMouseButtonDown(0))
-      {
-         Debug.Log("Test message");
-         Debug.Log(selectedItemGrid.GetTileGridPosition(Input.mousePosition));
-      }
+           if (selectedPackage == null)
+           {
+               selectedPackage = selectedItemGrid.PickUpItem(posOnGrid.x, posOnGrid.y);
+               if (selectedPackage != null)
+                   packageRectTransform = selectedPackage.GetComponent<RectTransform>();
+           }
+           else
+           {
+               selectedItemGrid.PlaceItem(selectedPackage, posOnGrid.x, posOnGrid.y);
+               selectedPackage = null;
+           }
+       }
    }
+   
 }
--- Assets/Scripts/Inventory/ItemGrid.cs
+++ Assets/Scripts/Inventory/ItemGrid.cs
@@ -7,14 +7,25 @@ public class ItemGrid : MonoBehaviour
 {
     const float tileSizeWidth = 32;
     const float tileSizeHeight = 32;
+    
+    [SerializeField] private int inventorySizeX;
+    [SerializeField]private int inventorySizeY;
 
+    [SerializeField] private GameObject itemPrefab;
+    
     RectTransform rectTransform;
     private Vector2 positionOnGrid = new Vector2();
     private Vector2Int tileGridPosition = new Vector2Int();
+
+    private Shipment[,] packageSlot;
     
     private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
+        Init(inventorySizeX, inventorySizeY);
+
+        Shipment item = Instantiate(itemPrefab).GetComponent<Shipment>();
+        PlaceItem(item,5, 2);
     }
 
     public Vector2Int GetTileGridPosition(Vector2 mousePosition)
@@ -25,5 +36,37 @@ public class ItemGrid : MonoBehaviour
         tileGridPosition.x = (int)(positionOnGrid.x / tileSizeWidth);
         tileGridPosition.y = (int)(positionOnGrid.y / tileSizeHeight);
         return tileGridPosition;
+    }
+
+    /// <summary>
+    /// Initializes Inventory grid
+    /// </summary>
+    /// <param name="width"></param>
+    /// <param name="height"></param>
+    void Init(int width, int height)
+    {
+        packageSlot = new Shipment[width, height];
+        Vector2 size = new Vector2(width * tileSizeWidth, height * tileSizeHeight);
+        rectTransform.sizeDelta = size;
+    }
+
+    public void PlaceItem(Shipment package, int xPos, int yPos)
+    {
+        RectTransform itemRectTransform = package.GetComponent<RectTransform>();
+        itemRectTransform.SetParent(rectTransform);
+        packageSlot[xPos, yPos] = package;
+
+        Vector2 itemPosition = new Vector2();
+        itemPosition.x = xPos * tileSizeWidth + tileSizeWidth / 2;
+        //inverted y calc because grid goes from Top left instead of unity's Bottom left
+        itemPosition.y = -(yPos * tileSizeHeight + tileSizeHeight / 2);
+        itemRectTransform.localPosition = itemPosition;
+    }
+
+    public Shipment PickUpItem(int xPos, int yPos)
+    {
+        Shipment toReturn = packageSlot[xPos,yPos];
+        packageSlot[xPos, yPos] = null;
+        return toReturn;
     }
 }
