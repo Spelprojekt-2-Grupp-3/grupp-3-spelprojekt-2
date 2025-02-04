4373fa6b50422265c2dbe4b6fba0770743e94635
Harxann <harxann@gmail.com>
2025-02-04 12:57:29 +0100

    Merged duplicated scripts

--- Assets/Scripts/Shipment.cs
+++ Assets/Scripts/Shipment.cs
@@ -3,11 +3,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 //Interface!
 public class Shipment : MonoBehaviour, IClick
 {
     private Player _player;
+    [SerializeField] private float weight;
+    [SerializeField, Tooltip("Size that the package takes up in the inventory")] 
+    private Vector2Int gridSize;
+    [Tooltip("Image for the package in the 2D inventory")]
+    public Image inventoryImage;
 
     private void Awake()
     {
