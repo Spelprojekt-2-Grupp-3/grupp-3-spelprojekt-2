2430c547e3d1dd7735df7c39318c20b64a7b2045
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-11 22:23:02 +0100

    BoatTilt and improvements

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -8,30 +8,30 @@ using UnityEngine.InputSystem;
 using FMOD;
 using Debug = UnityEngine.Debug;
 
-//using Debug = FMOD.Debug;
-
 [RequireComponent(typeof(Rigidbody), typeof(Animator), typeof(BuoyantObject))]
 public class BoatMovement : MonoBehaviour
 {
     [SerializeField, Range(0, 100)] [Tooltip("Always slows down until at this value when not pressing the gas")] 
     private float baseMoveSpeed = 10;
+    
     private float moveSpeed;
     [SerializeField, Range(1, 200)] [Tooltip("Acceleration")]
     private float acceleration = 100;
+    
     [SerializeField, Range(0f, 100f), Tooltip("Rotationspeed")] private float rotationSpeed = 1f;
     [SerializeField, Range(0f, 2500f)] private float maxSpeed = 2000f;
     [SerializeField, Range(0, 90)] private int tiltAngle = 25;
-    [SerializeField, Range(0f, 1f)] private float tiltSpeed = 1f;
+    [SerializeField, Range(0f, 10f)] private float tiltSpeed = 1f;
+    public FMODUnity.EventReference boatSoundEvent;
+    
+    private FMOD.Studio.EventInstance boatSound;
     private PlayerInputActions playerControls;
     private InputAction move;
     private InputAction gas;
     private InputAction look;
     private int moveDirection;
     private Rigidbody rb;
-    private float sinTime;
-    public FMODUnity.EventReference boatSoundEvent;
-    private FMOD.Studio.EventInstance boatSound;
-    private FMOD.Studio.PARAMETER_ID boatSoundId;
+    
     
     private void Awake()
     {
@@ -67,7 +67,12 @@ public class BoatMovement : MonoBehaviour
         boatSound.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject, rb));
         float boatSpeed = moveSpeed * 15 / maxSpeed;
         boatSound.setParameterByName("Boat Engine", boatSpeed);
-        if (!move.inProgress && !gas.inProgress) return;
+        if (!move.inProgress && !gas.inProgress && moveSpeed > baseMoveSpeed)
+        {
+            moveSpeed -= acceleration * Time.deltaTime;
+            if (moveSpeed < baseMoveSpeed) moveSpeed = baseMoveSpeed;
+        }
+        
         if (gas.inProgress)
         {
             moveSpeed += acceleration * Time.deltaTime;
@@ -76,30 +81,21 @@ public class BoatMovement : MonoBehaviour
                 moveSpeed = maxSpeed;
             }
         }
-        else if (moveSpeed > baseMoveSpeed)
-        {
-            moveSpeed -= acceleration * Time.deltaTime;
-            if (moveSpeed < baseMoveSpeed) moveSpeed = baseMoveSpeed;
-        }
     }
 
     private void FixedUpdate()
     {
         Vector3 euler = transform.localEulerAngles;
-        
-        euler.x += Mathf.Lerp(0, -tiltAngle * look.ReadValue<Vector2>().x, tiltSpeed);
-        
-        if (euler.x > tiltAngle)
-        {
-            euler.x = -tiltAngle * look.ReadValue<Vector2>().x;
-        }
+
+        float targetRot = -tiltAngle * look.ReadValue<Vector2>().x;
+        euler.x = Mathf.LerpAngle(euler.x, targetRot, tiltSpeed*Time.deltaTime);
         
         euler.y += rotationSpeed * look.ReadValue<Vector2>().x;
         rb.rotation = Quaternion.Euler(euler);
         
         if (!move.inProgress && !gas.inProgress) return;
         rb.velocity = new Vector3(
-            move.ReadValue<Vector2>().y * moveSpeed * transform.right.x, rb.velocity.y,
-            move.ReadValue<Vector2>().y * moveSpeed * transform.right.z);
+             move.ReadValue<Vector2>().y * moveSpeed * transform.right.x, rb.velocity.y,
+             move.ReadValue<Vector2>().y * moveSpeed * transform.right.z);
     }
 }
