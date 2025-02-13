2dcc6267229957e55e08076615f27cf2e11f2194
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-13 17:32:19 +0100

    Camera is always at back of boat and vertical boat tilt

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -20,7 +20,8 @@ public class BoatMovement : MonoBehaviour
     
     [SerializeField, Range(0f, 100f), Tooltip("Rotationspeed")] private float rotationSpeed = 1f;
     [SerializeField, Range(0f, 2500f)] private float maxSpeed = 2000f;
-    [SerializeField, Range(0, 90)] private int tiltAngle = 25;
+    [SerializeField, Range(0, 90)] private int sideTiltAngle = 25;
+    [SerializeField, Range(0, 90), Tooltip("How much the boat should tilt upwards at higher speeds")] private int frontTiltAngle = 25;
     [SerializeField, Range(0f, 10f)] private float tiltSpeed = 1f;
     public FMODUnity.EventReference boatSoundEvent;
     
@@ -87,12 +88,16 @@ public class BoatMovement : MonoBehaviour
     {
         Vector3 euler = transform.localEulerAngles;
 
-        float targetAngle = moveSpeed * tiltAngle / maxSpeed;
+        float targetAngle = moveSpeed * sideTiltAngle / maxSpeed;
         float targetRot = -targetAngle * move.ReadValue<Vector2>().x;
         euler.x = Mathf.LerpAngle(euler.x, targetRot, tiltSpeed*Time.deltaTime);
 
         float targetRotationSpeed = moveSpeed * rotationSpeed / maxSpeed;
         euler.y += targetRotationSpeed * move.ReadValue<Vector2>().x;
+        
+        float targetAngleZ = moveSpeed * frontTiltAngle / maxSpeed;
+        euler.z = Mathf.LerpAngle(euler.z, targetAngleZ, tiltSpeed*Time.deltaTime);
+        
         rb.rotation = Quaternion.Euler(euler);
         
         if (moveSpeed == 0) return;
