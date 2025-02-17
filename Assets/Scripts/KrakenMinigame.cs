c93ddbbf3b315bb8a9e06f1e29ecdbea8d8916db
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-17 21:47:34 +0100

    More minigame stuff

--- Assets/Scripts/KrakenMinigame.cs
+++ Assets/Scripts/KrakenMinigame.cs
@@ -9,22 +9,36 @@ public class KrakenMinigame : Minigames
     [SerializeField] private KrakenDifficulty krakenDifficulty;
     [SerializeField] private GameObject canvas;
     [SerializeField] private GameObject hpBar;
+    private List<GameObject> hpBarInstances = new List<GameObject>();
+    private Camera camera;
 
     private void Start()
     {
+        camera = Camera.main;
         StartMinigame();
     }
 
     public override void StartMinigame()
     {
-        var krakenInstance = Instantiate(kraken);
         var canvasInst = Instantiate(canvas);
-        var hpBarInst = Instantiate(hpBar, new Vector3(0, 0, 0), hpBar.transform.rotation, canvasInst.transform);
+        canvasInst.GetComponent<Canvas>().worldCamera = camera;
+        var krakenInstance = Instantiate(kraken, new Vector3(0, 0, 0), kraken.transform.rotation);
+        Vector3 barPos = new Vector3(krakenInstance.transform.position.x, krakenInstance.transform.position.y + 15, krakenInstance.transform.position.z);
+        var hpBarInst = Instantiate(hpBar, barPos, hpBar.transform.rotation, canvasInst.transform);
+        hpBarInstances.Add(hpBarInst);
         Events.stopBoat?.Invoke();
     }
 
     public override void StopMinigame()
     {
         Events.startBoat.Invoke();
+    }
+
+    private void Update()
+    {
+        foreach (var hpBarInst in hpBarInstances)
+        {
+            hpBarInst.transform.LookAt(camera.transform.position);
+        }
     }
 }
