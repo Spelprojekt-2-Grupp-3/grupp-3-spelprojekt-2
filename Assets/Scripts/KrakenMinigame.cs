d0f4eb4356b496cac3dd8f7275e81ca17ab0bdd0
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-18 00:34:41 +0100

    Even more minigame stuff

--- Assets/Scripts/KrakenMinigame.cs
+++ Assets/Scripts/KrakenMinigame.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class KrakenMinigame : Minigames
 {
@@ -9,8 +11,27 @@ public class KrakenMinigame : Minigames
     [SerializeField] private KrakenDifficulty krakenDifficulty;
     [SerializeField] private GameObject canvas;
     [SerializeField] private GameObject hpBar;
+    [SerializeField] private int tentacleCount;
     private List<GameObject> hpBarInstances = new List<GameObject>();
     private Camera camera;
+    private PlayerInputActions playerControls;
+    private InputAction minigame;
+
+    private void OnEnable()
+    {
+        minigame = playerControls.Boat.Minigame;
+        minigame.Enable();
+    }
+
+    private void OnDisable()
+    {
+        minigame.Disable();
+    }
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
 
     private void Start()
     {
@@ -20,13 +41,19 @@ public class KrakenMinigame : Minigames
 
     public override void StartMinigame()
     {
+        Events.stopBoat?.Invoke();
         var canvasInst = Instantiate(canvas);
         canvasInst.GetComponent<Canvas>().worldCamera = camera;
-        var krakenInstance = Instantiate(kraken, new Vector3(0, 0, 0), kraken.transform.rotation);
-        Vector3 barPos = new Vector3(krakenInstance.transform.position.x, krakenInstance.transform.position.y + 15, krakenInstance.transform.position.z);
-        var hpBarInst = Instantiate(hpBar, barPos, hpBar.transform.rotation, canvasInst.transform);
-        hpBarInstances.Add(hpBarInst);
-        Events.stopBoat?.Invoke();
+
+        for (int i = 0; i < tentacleCount; i++)
+        {
+            var krakenInstance = Instantiate(kraken, new Vector3(10*i, 0, 0), kraken.transform.rotation);
+            Vector3 barPos = new Vector3(krakenInstance.transform.position.x, krakenInstance.transform.position.y + 15, krakenInstance.transform.position.z);
+            var hpBarInst = Instantiate(hpBar, barPos, hpBar.transform.rotation, canvasInst.transform);
+            var tentacle = krakenInstance.AddComponent<Tentacle>();
+            hpBarInst.GetComponent<Image>().fillAmount = tentacle.hp / tentacle.maxHp;
+            hpBarInstances.Add(hpBarInst);
+        }
     }
 
     public override void StopMinigame()
@@ -40,5 +67,35 @@ public class KrakenMinigame : Minigames
         {
             hpBarInst.transform.LookAt(camera.transform.position);
         }
+
+        if (minigame.WasPerformedThisFrame())
+        { 
+            switch (minigame.ReadValue<Vector2>())
+            {
+                case Vector2 v when(v.x > 0):
+                    Debug.Log("right");
+                    break;
+                case Vector2 v when(v.x < 0):
+                    Debug.Log("left");
+                    break;
+                case Vector2 v when(v.y > 0):
+                    Debug.Log("up");
+                    break;
+                case Vector2 v when(v.y < 0):
+                    Debug.Log("down");
+                    break;
+            }
+        }
+    }
+}
+
+public class Tentacle : MonoBehaviour
+{
+    public int maxHp = 15;
+    public int hp;
+
+    private void Awake()
+    {
+        hp = maxHp;
     }
 }
