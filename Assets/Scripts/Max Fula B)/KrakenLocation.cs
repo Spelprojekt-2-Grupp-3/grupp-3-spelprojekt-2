2c514874a562debb353983b258a08b449b329121
Handensaken <maxmax.larsson@gmail.com>
2025-02-18 13:00:14 +0100

    Kraken moves. Particles not rendering hmm

--- Assets/Scripts/Max Fula B)/KrakenLocation.cs
+++ Assets/Scripts/Max Fula B)/KrakenLocation.cs
@@ -1,24 +1,40 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+//using System.Numerics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class KrakenLocation : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject kraken;
     private PlayerInputActions playerControls;
     private InputAction _inputDebug;
 
     [SerializeField]
-    private GameObject _tentacleBoatReference;
+    private GameObject _tentacleRefLeft;
+
+    [SerializeField]
+    private GameObject _tentacleRefRight;
 
     public GameObject player;
 
     private bool moving = false;
 
+    private enum _sideEnum
+    {
+        Left,
+        Right
+    }
+
+    private _sideEnum side;
+
     private void Awake()
     {
         playerControls = new PlayerInputActions();
+        side = _sideEnum.Left;
     }
 
     /* private void OnEnable()
@@ -28,35 +44,38 @@ public class KrakenLocation : MonoBehaviour
          _inputDebug.performed += _ => OnActivation();
      }*/
 
-    /* private void OnDisable()
-     {
-         _inputDebug.Disable();
-     }*/
+   /* private void OnDisable()
+    {
+        _inputDebug.Disable();
+        playerControls.Disable();
+    }*/
 
     void Update()
     {
         if (moving) //this hombrero sombrero doth not work MEGALUL
         {
-            Vector3 dir = (
-                transform.position - _tentacleBoatReference.transform.position
-            ).normalized;
-            transform.Translate(dir * 20f * Time.deltaTime);
-
-            if (
-                Vector3.Distance(transform.position, _tentacleBoatReference.transform.position)
-                <= 0.1f
-            )
+            Vector3 dir = (_tentacleRefLeft.transform.position - transform.position).normalized;
+            //  transform.position += (dir * 20f * Time.deltaTime);
+            transform.Translate(dir * 20 * Time.deltaTime, Space.World);
+            Debug.DrawRay(transform.position, dir, Color.red);
+            if (Vector3.Distance(transform.position, _tentacleRefLeft.transform.position) <= 00.1f)
             {
+                GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePositionY;
                 moving = false;
+                StartCoroutine(Timer(1, SpawnTent));
             }
         }
     }
 
     public void OnActivation() //just call to start shit
     {
+        Debug.Log("SHOULD set buoyancy false");
+        gameObject.GetComponent<BuoyantObject>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+
         Debug.Log("activating");
-        playerControls.Boat.Move.Disable();
-        StartCoroutine(Timer(1, SpawnTent));
+        Events.stopBoat?.Invoke();
+        // StartCoroutine(Timer(1, SpawnTent));
         moving = true;
     }
 
@@ -70,6 +89,16 @@ public class KrakenLocation : MonoBehaviour
 
     private void SpawnTent()
     {
+        GameObject SpawnedKraken = Instantiate(kraken, transform.position, Quaternion.identity);
+        if (side == _sideEnum.Left)
+        {
+            SpawnedKraken.transform.LookAt(_tentacleRefRight.transform);
+        }
+        else
+        {
+            SpawnedKraken.transform.LookAt(_tentacleRefLeft.transform);
+        }
         Debug.Log("Spawning");
+        gameObject.SetActive(false);
     }
 }
