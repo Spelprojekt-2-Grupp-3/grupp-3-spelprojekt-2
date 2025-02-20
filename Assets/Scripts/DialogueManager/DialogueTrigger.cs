846a22f4cbe4c6a65451a2afbe8b28c79c8106d7
Harxann <harxann@gmail.com>
2025-02-20 01:39:18 +0100

    Improved dialogue trigger

--- Assets/Scripts/DialogueManager/DialogueTrigger.cs
+++ Assets/Scripts/DialogueManager/DialogueTrigger.cs
@@ -2,46 +2,64 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class DialogueTrigger : MonoBehaviour, IClick
+public class DialogueTrigger : MonoBehaviour
 {
-   [Header("Dialogue Settings")]
-   [Tooltip("Drag the corresponding Ink JSON file for this NPC's dialogue. Each NPC should have a unique file.")]
-   [SerializeField] private TextAsset inkJSON;
+    [Header("Dialogue Settings")]
+    [Tooltip("Drag the corresponding Ink JSON file for this NPC's dialogue. Each NPC should have a unique file.")]
+    [SerializeField] private TextAsset inkJSON;
    
-   //Checks if the player is close to NPC
-   private bool playerInRange;
+    // Checks if the player is close to NPC
+    private bool playerInRange;
+    private PlayerInputActions _playerInput;
 
-   private void Awake()
-   {
-      playerInRange = false;
-   }
+    private void Awake()
+    {
+        playerInRange = false;
+        _playerInput = new PlayerInputActions();
+    }
 
-   //Temporary
+    private void OnEnable()
+    {
+        _playerInput.Enable();
+    }
 
-   //playerInRange isn't in the if-statement atm
-   public void OnClick()
-   {
-      //Starts dialogue if player is in range and no other dialogue is active
-      if (playerInRange && !DialogueManager.GetInstance().dialogueIsPlaying)
-      {
-         DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
-      }
-   }
+    private void OnDisable()
+    {
+        _playerInput.Disable();
+    }
 
-   private void OnTriggerEnter(Collider other)
-   {
-      if (other.CompareTag("Player"))
-      {
-         playerInRange = true;
-      }
-   }
+    private void Update()
+    {
+        // Check if player is inside the trigger and presses the "Interact" action
+        if (playerInRange && _playerInput.Player.Interact.WasPressedThisFrame())
+        {
+            StartDialogue();
+        }
+    }
 
-   private void OnTriggerExit(Collider other)
-   {
-      if (other.CompareTag("Player"))
-      {
-         playerInRange = false;
-      }
-   }
+    private void StartDialogue()
+    {
+        if (!DialogueManager.GetInstance().dialogueIsPlaying)
+        {
+            DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
 }
