4c132d3f7f7176a2986bb552e1d281c70d92ec5d
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-20 16:57:48 +0100

    Compass changes

--- Assets/Scripts/UI/Compass/Compass.cs
+++ Assets/Scripts/UI/Compass/Compass.cs
@@ -7,25 +7,48 @@ public class Compass : MonoBehaviour
     RectTransform rt;
 
     private Camera camera;
+    
+    private RectTransform canvasRect;
 
     void Start()
     {
         rt = GetComponent<RectTransform>();
         camera = Camera.main;
+        
+        Canvas canvas = transform.GetComponentInParent<Canvas>();
+        if (canvas != null)
+        {
+            canvasRect = canvas.GetComponent<RectTransform>();
+        }
+        else
+        {
+            Debug.LogError("Compass arrow is not a child of a Canvas.");
+        }
     }
 
     void Update()
     {
-        Vector3 objScreenPos = camera.WorldToScreenPoint(goal.transform.position);
+        if (canvasRect == null)
+            return;
         
-        Vector3 dir = (objScreenPos - rt.position).normalized;
-
-        if (objScreenPos.z < 0)
+        Vector3 targetScreenPos = camera.WorldToScreenPoint(goal.transform.position);
+        
+        if (targetScreenPos.z < 0)
         {
-            dir = -dir;
+            targetScreenPos *= -1f;
         }
         
-        float angle = Mathf.Rad2Deg * Mathf.Atan2(dir.y, dir.x);
+        Camera conversionCamera = (transform.GetComponentInParent<Canvas>().renderMode == RenderMode.ScreenSpaceOverlay)
+            ? null : camera;
+        
+        Vector2 targetLocalPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, targetScreenPos, conversionCamera, out targetLocalPos);
+        Vector2 compassLocalPos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, transform.position, conversionCamera, out compassLocalPos);
+        
+        Vector2 direction = (targetLocalPos - compassLocalPos).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         
         rt.localEulerAngles = new Vector3(0, 0, angle - 90);
     }
