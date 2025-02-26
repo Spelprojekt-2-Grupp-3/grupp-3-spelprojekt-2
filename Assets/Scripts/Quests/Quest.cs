5ab150f3b1565e7fb3b9813bd18525ce7a441a34
LindGabriel <114293540+LindGabriel@users.noreply.github.com>
2025-02-26 13:46:09 +0100

    stepped quests in works
    Removed port access

--- Assets/Scripts/Quests/Quest.cs
+++ Assets/Scripts/Quests/Quest.cs
@@ -5,15 +5,27 @@ using UnityEngine;
 
 public class Quest : MonoBehaviour
 {
-    public TMP_Text infoText;
     public TMP_Text titleText;
+    public TMP_Text infoText;
 
-    public void Set(QuestData data)
+    public void Set(QuestData data, int currentStep =0)
     {
+        if (data.steps > 0)
+        {
+            infoText.text = currentStep + "/" + data.steps + data.questText;
+        }
+        else
+        {
+            infoText.text = data.questText;
+        }
+        
         titleText.text = data.questTitle;
-        infoText.text = data.questText;
     }
-    
 
+    public void Set(string updatedInfo, string updatedTitle)
+    {
+        titleText.text = updatedTitle;
+        infoText.text = updatedInfo;
+    }
 
 }
