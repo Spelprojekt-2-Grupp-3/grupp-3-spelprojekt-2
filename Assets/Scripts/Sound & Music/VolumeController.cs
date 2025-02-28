4b018e676700bb7b7f53ab6ab2b89616c31f8321
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-02-28 19:50:42 +0100

    Made all volumecontrollers into one and fixed audiocanvas

---
+++ Assets/Scripts/Sound & Music/VolumeController.cs
@@ -0,0 +1,114 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using FMODUnity;
+using FMOD.Studio;
+
+public class VolumeController : MonoBehaviour
+{
+
+    // This script as a whole is used to put on a slider to control the master volume of the whole game. This is connected to the masterVCA in FMOD Studio.
+
+    // Path to Master VCA in FMOD
+    [SerializeField]
+    private string _masterVCAName = "vca:/Master";
+    
+    // Reference to slider
+    [SerializeField]
+    private Slider masterVolumeSlider;
+
+    private VCA _masterVCA;
+    
+    // Path to Music VCA in FMOD
+    [SerializeField]
+    private string _musicVCAName = "vca:/Music";
+
+    // Reference to slider
+    [SerializeField]
+    private Slider musicVolumeSlider;
+    
+    private VCA _musicVCA;
+    
+    // Path to SFX VCA in FMOD
+    [SerializeField]
+    private string _sfxVCAName = "vca:/SFX";
+
+    // Reference to slider
+    [SerializeField]
+    private Slider sfxVolumeSlider;
+
+    private VCA _sfxVCA;
+
+    private void Start()
+    {
+        // Fetch the VCA by name from FMOD
+        _masterVCA = RuntimeManager.GetVCA(_masterVCAName);
+        _musicVCA = RuntimeManager.GetVCA(_musicVCAName);
+        _sfxVCA = RuntimeManager.GetVCA(_sfxVCAName);
+
+        // If the VCA is valid then retrieve the saved volume setting from PlayerPrefs
+        if (_masterVCA.isValid())
+        {
+            float initialVolume = PlayerPrefs.GetFloat("MasterVolume", 1.0f);
+            _masterVCA.setVolume(initialVolume);
+            if (masterVolumeSlider != null)
+            {
+                // Set initial volume on the VCA
+                masterVolumeSlider.value = initialVolume;
+                masterVolumeSlider.onValueChanged.AddListener(SetMasterVolume);
+            }
+        }
+        
+        if (_musicVCA.isValid())
+        {
+            float initialVolume = PlayerPrefs.GetFloat("MusicVolume", 1.0f);
+            _musicVCA.setVolume(initialVolume);
+            if (musicVolumeSlider != null)
+            {
+                musicVolumeSlider.value = initialVolume;
+                musicVolumeSlider.onValueChanged.AddListener(SetMusicVolume);
+            }
+        }
+        
+        if (_sfxVCA.isValid())
+        {
+            float initialVolume = PlayerPrefs.GetFloat("SFXVolume", 1.0f);
+            _sfxVCA.setVolume(initialVolume);
+            if (sfxVolumeSlider != null)
+            {
+                sfxVolumeSlider.value = initialVolume;
+                sfxVolumeSlider.onValueChanged.AddListener(SetSFXVolume);
+            }
+        }
+    }
+
+    private void SetMasterVolume(float volume)
+    {
+        // Again, checks if VCA is valid before attempting to change anything (the volume in this case)
+        Debug.Log("setttingmastervolume");
+
+        if (_masterVCA.isValid())
+        {
+            _masterVCA.setVolume(volume);
+            PlayerPrefs.SetFloat("MasterVolume", volume);
+        }
+    }
+    
+    private void SetMusicVolume(float volume)
+    {
+        if (_musicVCA.isValid())
+        {
+            _musicVCA.setVolume(volume);
+            PlayerPrefs.SetFloat("MusicVolume", volume);
+        }
+    }
+    
+    private void SetSFXVolume(float volume)
+    {
+        if (_sfxVCA.isValid())
+        {
+            _sfxVCA.setVolume(volume);
+            PlayerPrefs.SetFloat("SFXVolume", volume);
+        }
+    }
+}
