a532f641b4d2a46971db6bd0da5ba8d4f56113b5
Handensaken <maxmax.larsson@gmail.com>
2025-02-28 15:06:19 +0100

    Minor adjustments to interactabliity between shit

--- Assets/Scripts/BoatMovement.cs
+++ Assets/Scripts/BoatMovement.cs
@@ -99,10 +99,10 @@ public class BoatMovement : MonoBehaviour
 
         if(pause.WasPressedThisFrame()) {
             if(Time.timeScale == 0) {
-                Time.timeScale = 1;
+                //Time.timeScale = 1;
             }
             else {
-                Time.timeScale = 0;
+                //Time.timeScale = 0;
             }
             pauseMenu.SetActive(!pauseMenu.activeSelf);
         }
