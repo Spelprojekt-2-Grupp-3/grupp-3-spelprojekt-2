150c8d304371c0c7c0c79c16af8818434074fbe6
Handensaken <maxmax.larsson@gmail.com>
2025-03-07 18:49:01 +0100

    Cleo Movement Input
    Missing backwards movement, otherwise mostly functional

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,16 +7,22 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody), typeof(Animator), typeof(BoxCollider))]
 public class PlayerMovement : MonoBehaviour
 {
+    private Animator _aniControl;
     private float moveSpeed = 0;
     private PlayerInputActions playerControls;
     private InputAction move;
-    [SerializeField, Range(0, 2000)] float maxMoveSpeed;
-    [SerializeField, Range(0, 300)] private float acceleration;
+
+    [SerializeField, Range(0, 2000)]
+    float maxMoveSpeed;
+
+    [SerializeField, Range(0, 300)]
+    private float acceleration;
     private InputAction interact;
     private Rigidbody rb;
-    
+
     private void Awake()
     {
+        _aniControl = GetComponent<Animator>();
         playerControls = new PlayerInputActions();
     }
 
@@ -40,7 +46,7 @@ public class PlayerMovement : MonoBehaviour
     {
         rb = GetComponent<Rigidbody>();
     }
-    
+
     void Update()
     {
         //If there is an active dialogue, freeze the player
@@ -48,30 +54,38 @@ public class PlayerMovement : MonoBehaviour
         //{
         //    return;
         //}
-        
-        if (!move.inProgress) return;
-        if (moveSpeed < maxMoveSpeed)
-        {
-            moveSpeed += acceleration * move.ReadValue<Vector2>().y;
-            if (moveSpeed > maxMoveSpeed)
-            {
-                moveSpeed = maxMoveSpeed;
-            }
-        }
+
+        /*if (!move.inProgress)
+            return;*/
+
+        Vector2 movementVector = move.ReadValue<Vector2>().normalized;
+        _aniControl.SetFloat("X", movementVector.x);
+        _aniControl.SetFloat("Y", movementVector.y);
+        /* if (moveSpeed < maxMoveSpeed)
+         {
+             moveSpeed += acceleration * move.ReadValue<Vector2>().y;
+             if (moveSpeed > maxMoveSpeed)
+             {
+                 moveSpeed = maxMoveSpeed;
+             }
+         }*/
     }
 
     private void FixedUpdate()
     {
-        Vector3 euler = transform.localEulerAngles;
-        
-        float targetRotationSpeed = 10f;
-        euler.y += targetRotationSpeed * move.ReadValue<Vector2>().x;
-        rb.rotation = Quaternion.Euler(euler);
-        
-        if (moveSpeed == 0) return;
-        rb.velocity = new Vector3(
-            moveSpeed * transform.forward.x, rb.velocity.y,
-            moveSpeed * transform.forward.z);
+        /* Vector3 euler = transform.localEulerAngles;
+ 
+         float targetRotationSpeed = 10f;
+         euler.y += targetRotationSpeed * move.ReadValue<Vector2>().x;
+         rb.rotation = Quaternion.Euler(euler);
+ 
+         if (moveSpeed == 0)
+             return;
+         rb.velocity = new Vector3(
+             moveSpeed * transform.forward.x,
+             rb.velocity.y,
+             moveSpeed * transform.forward.z
+         );*/
     }
 
     private void Interact(InputAction.CallbackContext context)
