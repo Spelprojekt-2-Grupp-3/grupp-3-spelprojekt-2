0714916c1291218d3cfb92a51c1968deccb8a169
Handensaken <maxmax.larsson@gmail.com>
2025-03-08 19:04:05 +0100

    hmmm why

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,8 @@ public class PlayerMovement : MonoBehaviour
     private Animator _aniControl;
     private float moveSpeed = 0;
     private PlayerInputActions playerControls;
-    private InputAction move, moveCam;
+    private InputAction move,
+        moveCam;
 
     [SerializeField, Range(0, 2000)]
     float maxMoveSpeed;
@@ -66,13 +67,16 @@ public class PlayerMovement : MonoBehaviour
 
         /*if (!move.inProgress)
             return;*/
+        Vector2 movementVector = move.ReadValue<Vector2>();
         if (moveCam.IsInProgress())
         {
             currentForward = camera.transform.forward;
             currentRight = camera.transform.right;
+            return;
         }
-        Vector2 movementVector = move.ReadValue<Vector2>().normalized;
-        
+        _aniControl.SetFloat("X", movementVector.x);
+        _aniControl.SetFloat("Y", movementVector.y);
+
         /* if (moveSpeed < maxMoveSpeed)
          {
              moveSpeed += acceleration * move.ReadValue<Vector2>().y;
@@ -85,34 +89,35 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-         float targetRotationSpeed = 10f;
-         Vector3 moveDirection = (currentForward * move.ReadValue<Vector2>().y) + (currentRight * move.ReadValue<Vector2>().x);
+        float targetRotationSpeed = 10f;
+        Vector3 moveDirection =
+            (currentForward * move.ReadValue<Vector2>().y)
+            + (currentRight * move.ReadValue<Vector2>().x);
 
-         if (moveDirection.sqrMagnitude > 0.01f)
-         {
-             float targetYRotation = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
-             rb.rotation =  Quaternion.Euler(0, targetYRotation, 0);
-             Vector3 euler = transform.localEulerAngles;
-         
-             _aniControl.SetFloat("X", euler.x);
-             _aniControl.SetFloat("Y", euler.y);
-         }
-         else
-         {
-             _aniControl.SetFloat("X", 0);
-             _aniControl.SetFloat("Y", 0);
-         }
-         
-         if (moveSpeed == 0)
-             return;
-         /*
-         rb.velocity = new Vector3(
-             1 * transform.forward.x,
-             rb.velocity.y,
-             1* transform.forward.z
-         );
-         */
-         
+        if (moveDirection.sqrMagnitude > 0.01f)
+        {
+            float targetYRotation = Mathf.Atan2(moveDirection.x, moveDirection.z) * Mathf.Rad2Deg;
+            rb.rotation = Quaternion.Euler(0, targetYRotation, 0);
+            Vector3 euler = transform.localEulerAngles;
+
+            _aniControl.SetFloat("X", euler.x);
+            _aniControl.SetFloat("Y", euler.y);
+        }
+        else
+        {
+            _aniControl.SetFloat("X", 0);
+            _aniControl.SetFloat("Y", 0);
+        }
+
+        if (moveSpeed == 0)
+            return;
+        /*
+        rb.velocity = new Vector3(
+            1 * transform.forward.x,
+            rb.velocity.y,
+            1* transform.forward.z
+        );
+        */
     }
 
     private void Interact(InputAction.CallbackContext context)
