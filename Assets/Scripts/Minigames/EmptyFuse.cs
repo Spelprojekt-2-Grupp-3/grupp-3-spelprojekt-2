a6f9af875c4ab6bcaaaf45d5f8467b11d10fc785
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-03-09 18:26:19 +0100

    Some more sigrid minigame stuff

---
+++ Assets/Scripts/Minigames/EmptyFuse.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyFuse : MonoBehaviour
+{
+    public bool emptyFuse;
+    public float voltage;
+}
