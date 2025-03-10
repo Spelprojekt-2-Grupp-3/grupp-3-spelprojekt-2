3a291eff635c5eb58035a928c3a6eea98035ed06
Handensaken <maxmax.larsson@gmail.com>
2025-03-10 02:49:51 +0100

    Some NPC setup

---
+++ Assets/Graphics/Animations/AnimationIdleAlternator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationIdleAlternator : MonoBehaviour
+{
+    private Animator _ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ani = GetComponent<Animator>();
+    }
+
+    public void Check()
+    {
+        float i = Mathf.RoundToInt(Random.Range(0, 6));
+        if (i == 5)
+        {
+            _ani.SetTrigger("Fidget");
+            Debug.Log("Fidget");
+        }
+        else
+        {
+            float f = Random.Range(8, 12) / 10f;
+            _ani.SetFloat("IdleSpeed", f);
+            Debug.Log(f);
+            Debug.Log("Repeat");
+        }
+    }
+}
