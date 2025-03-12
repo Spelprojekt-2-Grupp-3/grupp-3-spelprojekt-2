9820e0cac679fe242b860551f3a18bd31e892746
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-03-12 23:09:41 +0100

    Lots of UI stuff and cleanup

--- Assets/Scripts/UI/ControlsCheck.cs
+++ Assets/Scripts/UI/ControlsCheck.cs
@@ -10,6 +10,7 @@ public class ControlsCheck : MonoBehaviour
     [SerializeField] private GameObject moveControls;
     [SerializeField] private GameObject interactControls;
     [SerializeField] private GameObject gasControls;
+    [SerializeField] private GameObject reverseControls;
     [SerializeField] private CurrentInputIcons inputIcons;
     [SerializeField] private List<Image> interactIcons = new List<Image>();
 
@@ -28,6 +29,7 @@ public class ControlsCheck : MonoBehaviour
         moveControls.GetComponent<Image>().sprite = inputIcons.currentInputDevice.moveSprite;
         interactControls.GetComponent<Image>().sprite = inputIcons.currentInputDevice.interactSprite;
         gasControls.GetComponent<Image>().sprite = inputIcons.currentInputDevice.gasSprite;
+        reverseControls.GetComponent<Image>().sprite = inputIcons.currentInputDevice.reverseSprite;
         foreach (var icon in interactIcons)
         {
             icon.sprite = inputIcons.currentInputDevice.interactSprite;
