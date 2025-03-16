8939d964f8a64e2ab49a22114b63b9395d2d82dc
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-03-16 20:46:15 +0100

    Island markers

--- Assets/Scripts/UI/Compass/RadialCompass.cs
+++ Assets/Scripts/UI/Compass/RadialCompass.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,52 @@ using UnityEngine.UI;
 
 public class RadialCompass : MonoBehaviour
 {
+    public GameObject iconPrefab;
     [SerializeField] private RawImage compassImage;
     [SerializeField] private Transform boat;
+
+
+    [SerializeField] private List<IslandMarker> markers = new List<IslandMarker>();
     
+    private List<IslandMarker> islandMarkers = new List<IslandMarker>();
+
+    private float compassUnit;
+
+    private void Start()
+    {
+        compassUnit = compassImage.rectTransform.rect.width / 360f;
+        foreach (var marker in markers)
+        {
+            AddMarker(marker);
+        }
+    }
+
     void Update()
     {
         compassImage.uvRect = new Rect(boat.localEulerAngles.y/360f, 0f, 1f, 1f);
+
+        foreach (var marker in islandMarkers)
+        {
+            marker.image.rectTransform.anchoredPosition = GetPos(marker);
+        }
+    }
+
+    public void AddMarker(IslandMarker marker)
+    {
+        GameObject newMarker = Instantiate(iconPrefab, compassImage.transform);
+        marker.image = newMarker.GetComponent<Image>();
+        marker.image.sprite = marker.icon;
+        
+        islandMarkers.Add(marker);
+    }
+
+    Vector2 GetPos(IslandMarker marker)
+    {
+        Vector2 boatPos = new Vector2(boat.position.x, boat.position.z);
+        Vector2 boatForward = new Vector2(boat.forward.x, boat.forward.z);
+
+        float angle = Vector2.SignedAngle(marker.position - boatPos, boatForward);
+
+        return new Vector2(compassUnit * angle, 0f);
     }
 }
