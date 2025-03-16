796289469321ff0b8e92c19d2d49a79736f2dfa1
a23hanpu <144058026+a23hanpu@users.noreply.github.com>
2025-03-16 23:48:55 +0100

    Radial compass changes

--- Assets/Scripts/UI/Compass/RadialCompass.cs
+++ Assets/Scripts/UI/Compass/RadialCompass.cs
@@ -9,8 +9,7 @@ public class RadialCompass : MonoBehaviour
     public GameObject iconPrefab;
     [SerializeField] private RawImage compassImage;
     [SerializeField] private Transform boat;
-
-
+    private Camera camera;
     [SerializeField] private List<IslandMarker> markers = new List<IslandMarker>();
     
     private List<IslandMarker> islandMarkers = new List<IslandMarker>();
@@ -24,11 +23,13 @@ public class RadialCompass : MonoBehaviour
         {
             AddMarker(marker);
         }
+
+        camera = Camera.main;
     }
 
     void Update()
     {
-        compassImage.uvRect = new Rect(boat.localEulerAngles.y/360f, 0f, 1f, 1f);
+        compassImage.uvRect = new Rect(camera.transform.localEulerAngles.y/360f, compassImage.uvRect.y, compassImage.uvRect.width, compassImage.uvRect.height);
 
         foreach (var marker in islandMarkers)
         {
@@ -48,7 +49,7 @@ public class RadialCompass : MonoBehaviour
     Vector2 GetPos(IslandMarker marker)
     {
         Vector2 boatPos = new Vector2(boat.position.x, boat.position.z);
-        Vector2 boatForward = new Vector2(boat.forward.x, boat.forward.z);
+        Vector2 boatForward = new Vector2(camera.transform.forward.x, camera.transform.forward.z);
 
         float angle = Vector2.SignedAngle(marker.position - boatPos, boatForward);
 
