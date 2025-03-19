6b3898bf3be2eee8ce08082363becdff97dbb782
Handensaken <maxmax.larsson@gmail.com>
2025-03-19 15:07:06 +0100

    Particles no longer fuck themselves

--- Assets/Scripts/Max Fula B)/Particle Follow.cs
+++ Assets/Scripts/Max Fula B)/Particle Follow.cs
@@ -6,9 +6,18 @@ using UnityEngine;
 public class ParticleFollow : MonoBehaviour
 {
     public GameObject player;
+    Vector3 defaultLocalPos;
 
+    public enum type
+    {
+        back,
+        front,
+        side
+    }
 
-public float defaultX = 0;
+    public type particleType;
+
+    public float UpperLimit = 0;
     private ParticleSystem particleSystem;
 
     // Start is called before the first frame update
@@ -18,7 +27,8 @@ public float defaultX = 0;
         defaultLifetime = particleSystem.startLifetime;
         defaultEmissionRate = particleSystem.emissionRate;
         defaultRotation = particleSystem.transform.rotation;
-        defaultX = transform.position.x;
+        //defaultX = transform.position.x;
+        defaultLocalPos = transform.localPosition;
     }
 
     float defaultLifetime;
@@ -34,29 +44,63 @@ public float defaultX = 0;
     // Update is called once per frame
     void Update()
     {
-
-        
-
         //We get the active movement speed of the player
         playerMovementSpeed = player.GetComponent<BoatMovement>().moveSpeed;
         if (playerMovementSpeed > 0.1f)
         {
             //We create a scalar value from the maximum possible boat speed (boosting)
             scalarValue = playerMovementSpeed / 700;
-            particleSystem.enableEmission = true; //legacy particle code because it fucking works
+
+            switch (particleType)
+            {
+                case type.back:
+                {
+                    particleSystem.enableEmission = true; //legacy particle code because it fucking works
+                    break;
+                }
+                case type.front:
+                {
+                    if (playerMovementSpeed > 345)
+                    {
+                        particleSystem.enableEmission = true;
+                    }
+                    else
+                    {
+                        particleSystem.enableEmission = false;
+                    }
+                    break;
+                }
+                case type.side:
+                {
+                    if (playerMovementSpeed > 250)
+                    {
+                        particleSystem.enableEmission = true;
+                    }
+                    else
+                        particleSystem.enableEmission = false;
+                    break;
+                }
+                default:
+                {
+                    Debug.LogWarning("ParticleType not defined");
+                    break;
+                }
+            }
+
             var emi = particleSystem.emission; //we reference the emission
             emi.rateOverTime = defaultEmissionRate * scalarValue; //we update the emmission based on movement speed, faster speed means faster emmission
 
             particleSystem.startLifetime = defaultLifetime * (1.25f - scalarValue); //We do basically the same with lifetime but reversed. The faster you move the faster your particles die
 
             //Clamp particle movement
-            if (transform.position.y < lowerLimit || transform.position.y > lowerLimit + 1.5f)
+            if (transform.position.y < lowerLimit || transform.position.y > UpperLimit)
             {
                 transform.position = new Vector3(
                     transform.position.x,
                     lowerLimit,
                     transform.position.z
                 );
+                transform.localPosition = defaultLocalPos;
             }
         }
         else
