40fd5b54eb4f7a00a885f087abe20e089210b5fd
Harxann <harxann@gmail.com>
2025-03-25 17:14:59 +0100

    Hides npc icon during dialogue

--- Assets/Scripts/ActivateElementOnTrigger.cs
+++ Assets/Scripts/ActivateElementOnTrigger.cs
@@ -6,8 +6,44 @@ using UnityEngine;
 public class ActivateElementOnTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject obj;
+    private bool playerInsideTriggerZone = false;
+
+    private void Update()
+    {
+        if (!playerInsideTriggerZone) return;
+        
+        bool dialogueActive = DialogueManager.GetInstance().dialogueIsPlaying;
+        bool cooldownActive = !DialogueManager.GetInstance().canStartNewDialogue;
+
+        // If dialogue or cooldown is active, hide the icon
+        if (dialogueActive || cooldownActive)
+        {
+            if (obj.GetComponent<Canvas>())
+            {
+                obj.GetComponent<Canvas>().enabled = false;
+            }
+            else
+            {
+                obj.SetActive(false);
+            }
+        } 
+        else
+        {
+            if (obj.GetComponent<Canvas>())
+            {
+                obj.GetComponent<Canvas>().enabled = true;
+            }
+            else
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        playerInsideTriggerZone = true;
+        
         if (obj.GetComponent<Canvas>())
         {
             obj.GetComponent<Canvas>().enabled = true;
@@ -20,6 +56,8 @@ public class ActivateElementOnTrigger : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
+        playerInsideTriggerZone = false;
+        
         if (obj.GetComponent<Canvas>())
         {
             obj.GetComponent<Canvas>().enabled = false;
